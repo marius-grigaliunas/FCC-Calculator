5f0241b8b6df6778cf121650cfc36dfc35cd95f7
Marius Grigaliūnas <77457402+marius-grigaliunas@users.noreply.github.com>
2025-02-20 12:51:35 +0000

    Shift End

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -2,37 +2,44 @@ import React from "react";
 
 
 export const Calculator : React.FC = () => {
+    
+    const formul
+
+    const clear = () => {
+
+    }
 
     return (
-        <div id="calculator" className="border h-100 w-66 flex flex-col p-1">
-            <div id="display" className="border h-20 border-red-500 mb-2">
-
+        <div id="calculator" className="border h-100 w-80 flex flex-col p-1">
+            <div id="display-container" className=" bg-black h-20 mb-2">
+                <div id="formula-display" className="h-10 flex justify-end p-1 text-white text-2xl">2+2</div>
+                <div id="immediate-display" className="h-10 flex justify-end p-1 text-white text-2xl">4</div>
             </div>
-            <div id="buttonContainer" className="border h-80 border-green-400 grid grid-cols-4 w-64">
+            <div id="buttonContainer" className="h-80 grid grid-cols-4">
 
-                <div className="bg-red-700 text-white p-4 col-span-2 flex justify-center items-center">AC</div>
-                <div className="bg-gray-500 text-white p-4 flex justify-center items-center">/</div>
-                <div className="bg-gray-500 text-white p-4 flex justify-center items-center">x</div>
+                <div id="clear" className="bg-red-700 text-white p-4 col-span-2 flex justify-center items-center">AC</div>
+                <div id="divide" className="bg-gray-500 text-white p-4 flex justify-center items-center">/</div>
+                <div id="multiply" className="bg-gray-500 text-white p-4 flex justify-center items-center">x</div>
 
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">7</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">8</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">9</div>
-                <div className="bg-gray-500 text-white p-4 flex justify-center items-center">-</div>
+                <div id="seven" className="bg-gray-600 text-white p-4 flex justify-center items-center">7</div>
+                <div id="eight" className="bg-gray-600 text-white p-4 flex justify-center items-center">8</div>
+                <div id="nine" className="bg-gray-600 text-white p-4 flex justify-center items-center">9</div>
+                <div id="subtract" className="bg-gray-500 text-white p-4 flex justify-center items-center">-</div>
             
             
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">4</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">5</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">6</div>
-                <div className="bg-gray-500 text-white p-4 flex justify-center items-center">+</div>
+                <div id="four" className="bg-gray-600 text-white p-4 flex justify-center items-center">4</div>
+                <div id="five" className="bg-gray-600 text-white p-4 flex justify-center items-center">5</div>
+                <div id="six" className="bg-gray-600 text-white p-4 flex justify-center items-center">6</div>
+                <div id="add" className="bg-gray-500 text-white p-4 flex justify-center items-center">+</div>
 
                 
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">1</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">2</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">3</div>
-                <div className="bg-blue-800 text-white row-span-2 p-4 flex justify-center items-center">=</div>
+                <div id="one" className="bg-gray-600 text-white p-4 flex justify-center items-center">1</div>
+                <div id="two" className="bg-gray-600 text-white p-4 flex justify-center items-center">2</div>
+                <div id="three" className="bg-gray-600 text-white p-4 flex justify-center items-center">3</div>
+                <div id="equals" className="bg-blue-800 text-white row-span-2 p-4 flex justify-center items-center">=</div>
 
-                <div className="bg-gray-600 text-white col-span-2 p-4 flex justify-center items-center">0</div>
-                <div className="bg-gray-600 text-white p-4 flex justify-center items-center">.</div>
+                <div id="zero" className="bg-gray-600 text-white col-span-2 p-4 flex justify-center items-center">0</div>
+                <div id="decimal" className="bg-gray-600 text-white p-4 flex justify-center items-center">.</div>
                 
             </div>
         </div>
