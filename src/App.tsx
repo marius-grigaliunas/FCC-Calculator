5449b3a780dd9c04f4636893be9f5a883b8e8a30
Marius Grigaliunas <cia.marius.grigaliunas@gmail.com>
2025-02-20 21:00:34 +0100

    re-initialized the project in local dev environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { Calculator } from "./Components/Calculator.tsx"
+
 
 function App() {
 
-  // different colors gradient
-
   return (
     <>
-      <div className="flex justify-center items-center w-full h-screen">
-        <div className="p-4 border ">
-          <h1>Calculator </h1>
-          <Calculator/>
-        </div>
-      </div>
+    
     </>
   )
 }
